34ec2b697788bb63fb8edd9542899e74c31295de
marta <mamoran@teys.es>
2023-01-18 10:12:30 +0100

    first steps ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const name:string = "Marta"
+const age:number = 12
+const isStudent:boolean = false
+let hobbies:string[] = ["read", "dance"]
+let role:[number, string] //puede tener distintos tipos de datos
+
+//para que una variable admita dos tipos de datos
+let ageMarta: number | string
+
+type Person = {
+  name: string,
+  age: number // si quiero que sea opcional pongo age?
+}
+
+const person: Person = {
+  name:"Marta",
+  age: 24
+}
+
+/*
+MANERAS DE DEFINIR UNA FUNCIÓN EN TS
+
+function printName_1(name: string) {
+  console.log("Hola ", name)
+}
+
+const printName_2 = (name: string): string => {
+  return `Hi ${name}`;
+}
+
+let printName_3: (name: string) => number;
+
+let myName = (name:string): string => `Hi ${name}`
+
+*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
