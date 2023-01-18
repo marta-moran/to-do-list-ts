55db90cdb1b479584b20992dcd78181954dc871d
marta <martamoranl06@gmail.com>
2023-01-18 12:34:36 +0100

    input component

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import './InputStyles.css'
+import { useState } from "react";
 
 const InputField = () => {
+const [todo, setTodo] = useState<string>("")
+
+/* 
+si pasase props del componente padre (se crea el useState del todo,setTodo en el componente HomePage)
+Se crea una interfaz porque hay que defeinir el tipo de dato que es  cada uno (todo es un string pero el setTodo es una fucnión )
+interface Props {
+    todo:string,
+    setTodo: React.Dispatch<React.SetStateAction<String>>
+}
+
+se pasarían las props por la función --> const inputField = ({todo, setTodo}: Props)
+*/
+
+
   return (
-    <div>InputField</div>
+   <form className='form'>
+        <input type='input' placeholder='Enter a task' className='input__box' onChange={(e) => setTodo(e.target.value) } />
+        <button className='input_submit' type='submit'>Go</button>
+   </form>
   )
 }
+
 
 export default InputField
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import InputField from "../../components/InputField/InputField";
 
 function HomePage() {
+  //const [todo, setTodo] = useState<string>("") //si quisera usar string o number useState<string | number>("")
   return (
     <div>
       <header className="App-header">
         <h1>Taskify</h1>
       </header>
-      <InputField></InputField>
+      <InputField />
     </div>
   );
 }
