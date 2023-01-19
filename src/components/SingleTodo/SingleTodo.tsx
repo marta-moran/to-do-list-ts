a8efd9424fb06299363a31110edc4db25d2a5bb8
marta <martamoranl06@gmail.com>
2023-01-19 16:14:45 +0100

    actions (delete, done)

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -5,6 +5,8 @@ import { AiFillEdit } from "react-icons/ai";
 import { AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import Todolist from "../Todolist/Todolist";
+import { useState } from "react";
+import { CgCheckR } from "react-icons/cg";
 
 type Props = {
   todo: Todo;
@@ -13,37 +15,43 @@ type Props = {
 };
 
 function SingleTodo({ todo, todos, setTodos }: Props) {
+  const [edit, setEdit] = useState(false);
 
   const handleEditAction = (id: number) => {
-    
-    todos.map(todo => {
-      if(todo.id === id) {
-        
-      }
-    //   todo.id === id ? {...todo, isDone: false} : todo
-    //   setTodos(todos)
-     })
-  }
+    setEdit(true);
+  };
 
   const handleDeleteAction = (id: number) => {
-    // setTodos(todos.map(todo => todo.id !== id ? ))
-  }
-  
-  const handleDoneAction = (id: number) => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
-  }
+    setTodos(todos.filter((element) => element.id !== id));
+  };
 
+  const handleDoneAction = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
 
   return (
     <form className="todos__singles">
+      { 
+
+      edit ? 
+      <div>
+        <input type="text"></input><span className="icon"><CgCheckR /></span>
+      </div> : (
+        
+      <div>
       {
-        todo.isDone ? (
-          <s className="single__text">{todo.todo}</s>
-        ) : (
-          <span className="single__text">{todo.todo}</span>
-        )
-      }
-      {/* <span className="single__text">{todo.todo}</span> */}
+      todo.isDone ? (
+        <span className="single__text" style={{textDecoration: "line-through",color: "rgb(239, 38, 109)"}}>
+          {todo.todo}
+        </span>
+      ) : (
+        <span className="single__text">{todo.todo}</span>
+      )}
+
       <div>
         <span className="icon" onClick={() => handleEditAction(todo.id)}>
           <AiFillEdit color="rgb(198, 198, 243)"></AiFillEdit>
@@ -52,11 +60,25 @@ function SingleTodo({ todo, todos, setTodos }: Props) {
           <AiFillDelete color="rgb(250, 109, 109)"></AiFillDelete>
         </span>
         <span className="icon" onClick={() => handleDoneAction(todo.id)}>
-            <MdDone color="rgb(165, 233, 165)"></MdDone>
+          <MdDone color="rgb(165, 233, 165)"></MdDone>
         </span>
       </div>
+    </div>
+      )
+      }
     </form>
+
+
+
+
+
   );
 }
 
 export default SingleTodo;
+
+// {
+
+//   edit ? <input></input> : null
+
+// }
